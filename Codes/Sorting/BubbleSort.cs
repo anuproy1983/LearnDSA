b356a296a39cfbb95be5d3ba0709c1f00872e399
Anup-PC <Anup-PC@DESKTOP-4FSA8O2>
2025-10-22 23:01:07 -0500

    Added time and space complexity

--- Codes/Sorting/BubbleSort.cs
+++ Codes/Sorting/BubbleSort.cs
@@ -19,15 +19,20 @@ namespace LearnDSA.Codes.Sorting
             //and find the smallet one and swap it with big number.
             //2.Repeast the same process with all element  and return the sorted array to print.
             //[64, 34, 25, 12, 22, 11, 90]
+            //Space complexity: O(1)
+            //Bubble Sort sorts the array in place.
+            //It does not use any extra data structures or allocate additional memory proportional to the input size.
+            //Only a few variables (like loop counters and possibly a temporary variable for swapping) are used, which do not depend on the size of the input array.
+            //Time complexity: O(n^2)
 
-            for (int i = 0; i < nums.Length - 1; i++)
+            for (int i = 0; i < nums.Length - 1; i++) //O(n)
             {
-                for (int j = 0; j < nums.Length - 1; j++)
+                for (int j = 0; j < nums.Length - 1; j++) //O(n)
                 {
-                    if (nums[j] > nums[j + 1])
+                    if (nums[j] > nums[j + 1]) //O(1)
                     {
                         //using tuple. no temp variable needed here
-                        (nums[j + 1], nums[j]) = (nums[j], nums[j + 1]);
+                        (nums[j + 1], nums[j]) = (nums[j], nums[j + 1]);//O(1)
                     }
                 }
             }
